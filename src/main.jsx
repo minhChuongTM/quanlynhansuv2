6ca9e739aa620acfa413ba07607b300c9dc55c3e
Đỗ Kỳ Anh <164603696+dokyanh220@users.noreply.github.com>
2025-11-02 13:13:34 +0700

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,37 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/srore.js";
+import store from "./redux/store.js";
+import { ConfirmProvider } from "material-ui-confirm";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+            <ConfirmProvider
+            defaultOptions={{
+                title: 'Xác nhận',
+                confirmationText: 'Xác nhận',
+                cancellationText: 'Hủy',
+                confirmationButtonProps: { variant: 'contained', color: 'primary' },
+                cancellationButtonProps: { variant: 'outlined' }
+            }}
+            >
+            <App />
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            </ConfirmProvider>
             </BrowserRouter>
         </Provider>
     </StrictMode>
